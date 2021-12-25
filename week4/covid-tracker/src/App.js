1a7be785540b91fba54aaa039f28dd6005c2cfd0
Marzieh <mar.31069@gmail.com>
2021-12-25 18:53:54 +0330

    Add cards

--- week4/covid-tracker/src/App.js
+++ week4/covid-tracker/src/App.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import { Cards, Charts, CountryPicker } from './components';
 import styles from './App.module.css';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState();
+
+  // Fetch Data
+  const url = 'https://covid19.mathdro.id/api';
+  useEffect(() => {
+    async function fetchData() {
+      const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios(url);
+
+      const covidData = {
+        confirmed,
+        recovered,
+        deaths,
+        lastUpdate
+      }
+      setData(covidData);
+    }
+    fetchData();
+  }, [])
+
   return (
     <div style={styles.App}>
       <header style={styles.header}>
@@ -14,7 +35,7 @@ function App() {
       </header>
 
       <main>
-        <Cards />
+        {data && <Cards data={data}/>}
         <CountryPicker />
         <Charts />
       </main>
--- week4/covid-tracker/src/components/Cards/Cards.jsx
+++ week4/covid-tracker/src/components/Cards/Cards.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import styles from './Cards.module.css';
+import cx from 'classnames'
+import { Card, CardContent, Typography, Grid  } from '@material-ui/core';
+import CountUp from 'react-countup';
 
-const Cards = () => {
+const Cards = ( {data: { confirmed, recovered, deaths, lastUpdate }}) => {
 
   return (
-    <h1>Cards</h1>
+    <div style={styles.container}>
+      <Grid container spacing={3} justifyContent='center'>
+        <Grid item element={Card} xs={10} md={3} className={cx(styles.box, styles.card, styles.infected)}>
+          <CardContent>
+            <Typography variant='h5' color='textSecondary' gutterBottom>Infected</Typography>
+            <Typography varian='h5'>
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=','
+              />
+            </Typography>
+            <Typography color='textSecondary'>{new Date(lastUpdate).toLocaleString('en-US', {year:'numeric', month:'short', day: 'numeric', weekday:'short'})}</Typography>
+            <Typography varian='body2'>Number of active cases of Covid-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item element={Card} xs={10} md={3} className={cx(styles.box, styles.card, styles.recovered)}>
+          <CardContent>
+            <Typography variant='h5' color='textSecondary' gutterBottom>Recovered</Typography>
+            <Typography varian='h5'>
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={2.5}
+                separator=','
+              />
+            </Typography>
+            <Typography color='textSecondary'>{new Date(lastUpdate).toLocaleString('en-US', {year:'numeric', month:'short', day: 'numeric', weekday:'short', hours: true})}</Typography>
+            <Typography varian='body2'>Number of recoveries from Covid-19</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item element={Card} xs={10} md={3} className={cx(styles.box, styles.card, styles.deaths)}>
+          <CardContent>
+            <Typography variant='h5' color='textSecondary' gutterBottom>Deaths</Typography>
+            <Typography varian='h5'>
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                separator=','
+              />
+            </Typography>
+            <Typography color='textSecondary'>{new Date(lastUpdate).toLocaleString('en-US', {year:'numeric', month:'short', day: 'numeric', weekday:'short', hours: true})}</Typography>
+            <Typography varian='body2'>Number of deaths caused by Covid-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
